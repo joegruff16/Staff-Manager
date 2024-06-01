dccaf56fbede98d9e5b4b697d9d720f410686d78
Joseph Ruff <joegruff16@gmail.com>
2024-06-01 16:00:36 -0400

    Finished creating user prompts that will ask the user if they would like to view all departments, view all employees, add a department, etc. Fixed bug that prevented the prompt from starting. Now view all departments and add a department doesn't work.

--- server.js
+++ server.js
@@ -25,50 +25,69 @@ pool.connect();
 
 // Prompt the user asking them what they want to do using inquirer
 // Prompt would list the options to view all departments, view all roles, etc. 
-const start = () => {
-    inquirer
+const start = async () => {
+    await inquirer
         .prompt([
             {
                 name: 'Choice',
                 type: 'list',
                 message: 'Please make a selection',
                 choices: [
-                    'View all roles',
                     'View all departments',
-                    'View all employees'
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add a employee',
+                    'Update an employee role'
                 ]
 
-
             }
-        ]).then((response) => {
-            console.log(response)
-        })
-}
+        ]).then(async (response) => {
+            console.log(response);
+            if (response.Choice === 'View all departments') {
+                await viewAllDepartments();
+                console.log(response);
+            }
+            // Add similiar logic for remaining functions
+            if (response.Choice === 'Add a department') {
+                await addDepartment();
+            }
+        });
+    // Examples:
+    // View department function and rinse and repeat for all of the tables
+    // Create a function here that will enable user to view all departments
+    const viewAllDepartments = async () => {
+        try {
+            // Connect to database
+            const client = await pool.connect()
+            // Get departments from database
+            const department = await client.query(`select * from department`)
+            console.log(department.rows)
+        } catch (err) {
+            console.error(err)
+        }
 
-// Examples:
-// View department function and rinse and repeat for all of the tables
-// Create a function here that will enable user to view all departments
-const viewAllDepartments = async () => {
+    }
+};
+
+start();
+
+// Create new function to addDepartment
+const addDepartment = async () => {
     try {
         // Connect to database
         const client = await pool.connect()
         // Get departments from database
-        const department = await client.query(`select * from department`)
-        console.log(department)
-
-        // Create variables to store id and name
-        const id = 0;
-        const name = '';
-        // Add a department
-        const addDepartment = await client.query(`INSERT INTO department (${id}, ${name})
-    VALUES (${id}, ${name});`)
-        console.log(addDepartment)
+        const addNewDepartment = await client.query(`INSERT INTO department (id, name)
+    VALUES (1, ('')`)
+        console.log(addNewDepartment)
     } catch (err) {
-        console.error(error)
+        console.error(err)
     }
 
-}
-viewAllDepartments();
+};
+
 
 // Create a function that will enable user to add a department
 // Since department is the most simple create the schema, seeds, and the function for the departments. Then try with roles and lastly to the employees

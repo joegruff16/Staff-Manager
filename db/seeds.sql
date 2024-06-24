6fef65e5c84457d1989227f5f10c70f79216e490
Joseph Ruff <joegruff16@gmail.com>
2024-06-24 19:36:09 -0400

    Added a .env file which is probably not necessary. Added seed data to my database.

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,27 @@ VALUES ('Skills and Development'),
        ('Human Resources'),
        ('Information Technology'),
        ('Management');
+
+-- Role seeds
+INSERT INTO role (title, salary, department_id)
+VALUES ('Software Developer', 85000, 3),
+         ('QA Analyst', 75000, 3),
+         ('HR Generalist', 90000, 2),
+         ('HR Specialist', 70000, 2),
+         ('Training Specialist', 65000, 1),
+         ('Development Specialist', 80000, 1),
+         ('Training Manager', 85000, 4),
+         ('HR Manager', 90000, 4),
+         ('Product Manager', 110000, 4);
+
+-- Employee seeds
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Shepard', 1, NULL),
+       ('Jane', 'Shelby', 2, 1),
+       ('Jim', 'Gregg', 3, 2),
+       ('Jill', 'Torresdale', 4, 3),
+       ('Frank', 'Criniti', 5, 4),
+       ('Francesca', 'Marino', 6, 5),
+       ('Alex', 'Gentry', 7, 6),
+       ('Suzanne', 'Kalb', 8, 7),
+       ('Brian', 'Peppers', 9, 8);

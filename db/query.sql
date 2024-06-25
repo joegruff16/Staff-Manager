b476f29e113995b0cfa5628f4bbf22a2e2aec957
Joseph Ruff <joegruff16@gmail.com>
2024-06-24 21:02:11 -0400

    Added all remaining queries but I think the update employee role query is incorrect.

--- db/query.sql
+++ db/query.sql
@@ -2,4 +2,21 @@
 select * from department;
 
 -- Add a department table
--- INSERT INTO department (id, name) VALUES (1, 2);
+INSERT INTO department (name)
+
+-- Displays all roles
+select * from role;
+
+-- Add a role
+INSERT INTO role (title, salary, department_id)
+
+-- Displays all employees
+select * from employee;
+
+-- Add an employee
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+
+-- Update an employee role
+UPDATE employee
+SET role_id = (SELECT id FROM role WHERE title = 'HR Manager')
+WHERE id = 4;
